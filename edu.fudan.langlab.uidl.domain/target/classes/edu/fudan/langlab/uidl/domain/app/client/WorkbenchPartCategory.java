cf83eff7bf3f46c4e8f2126455ae88f765f5a176
rock <rock.guo@me.com>
2015-05-10 20:03:46 +0800

    add .class file

---
+++ edu.fudan.langlab.uidl.domain/target/classes/edu/fudan/langlab/uidl/domain/app/client/WorkbenchPartCategory.java
@@ -0,0 +1,11 @@
+package edu.fudan.langlab.uidl.domain.app.client;
+
+import com.google.gwt.resources.client.ImageResource;
+
+public interface WorkbenchPartCategory {
+	String getCategoryId();
+
+	String getCategoryName();
+	
+	ImageResource getImage();
+}
